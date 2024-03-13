b61a117a76ab5ec7ddfd07e04fc90de8418d3da4
KAJI-JAPAN <kurowassan1206@gmail.com>
2024-03-14 00:31:37 +0900

    WIP

--- lib/main.dart
+++ lib/main.dart
@@ -38,12 +38,13 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
+    var pair = appState.current;
 
     return Scaffold(
       body: Column(
         children: [
           Text('A random AWENSOME idea:'),
-          Text(appState.current.asLowerCase),
+          BigCard(pair: pair),
           ElevatedButton(onPressed: () {
             appState.getNext();
           },
@@ -51,6 +52,27 @@ class MyHomePage extends StatelessWidget {
           )
         ],
       ),
+    );
+  }
+}
+
+class BigCard extends StatelessWidget {
+  const BigCard({
+    super.key,
+    required this.pair,
+  });
+
+  final WordPair pair;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    return Card(
+      color: theme.colorScheme.primary;
+      child: Padding (
+        padding: const EdgeInsets.all(20),
+        child: Text(pair.asLowerCase),
+    )
     );
   }
 }
